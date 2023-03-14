eb1edaf8e8d7a6e21a2c98f454b6aef165f5de3c
kanekotic <me@kanekotic.com>
2023-03-14 15:44:58 +0100

    ALVARO | fix typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/resfql/website/tree/main/',
+            'https://github.com/restfql/website/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/resfql/website/tree/main/',
+            'https://github.com/restfql/website/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
